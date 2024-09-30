7e29e405cb73d3e01789492ad2e56fed81326dfe
Naw Joyful Awgyi <nawjoyful.awgyi@gmail.com>
2024-09-30 14:16:41 +0700

    Github CICD

--- src/main/java/se331/AdvancedCalculator.java
+++ src/main/java/se331/AdvancedCalculator.java
@@ -1,14 +1,14 @@
 package se327;
 
- public class AdvancedCalculator extends se327.Calculator {
- public double power(int base, int exponent) {
- return Math.pow(base, exponent);
- }
+public class AdvancedCalculator extends se327.Calculator {
+    public double power(int base, int exponent) {
+        return Math.pow(base, exponent);
+    }
 
- public double sqrt(int a) throws IllegalArgumentException {
- if (a < 0) {
- throw new IllegalArgumentException("Cannot calculate square root of a negative number.");
- }
- return Math.sqrt(a);
- }
- }
+    public double sqrt(int a) throws IllegalArgumentException {
+        if (a < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number.");
+        }
+        return Math.sqrt(a);
+    }
+}
